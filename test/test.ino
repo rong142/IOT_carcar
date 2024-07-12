2b7e4b34ac5973697cc724dc8a6c54a3f32c1bc3
rong142 <C109118142@nkust.edu.tw>
2024-07-12 13:01:11 +0800

    test_0712

--- test/test.ino
+++ test/test.ino
@@ -2,7 +2,7 @@
 char cmd;
 
 #include <WiFi.h>
-#include <PubSubClient.h>  //請先安裝PubSubClient程式庫
+#include <PubSubClient.h> //請先安裝PubSubClient程式庫
 
 // 設定啟動或停止馬達的參數
 // 一開始先設定成「停止」
@@ -23,10 +23,10 @@ char ssid[] = "C220MIS";
 char password[] = "misc220c220";
 
 // &取紙器=取得變數在記憶體中的位置 *指向氣=指向記憶體開始的位置------ 以下修改成你MQTT設定 ------
-char* MQTTServer = "mqttgo.io";  //免註冊MQTT伺服器
-int MQTTPort = 1883;             //MQTT Port
-char* MQTTUser = "";             //不須帳密
-char* MQTTPassword = "";         //不須帳密
+char* MQTTServer = "mqttgo.io";//免註冊MQTT伺服器
+int MQTTPort = 1883;//MQTT Port
+char* MQTTUser = "";//不須帳密
+char* MQTTPassword = "";//不須帳密
 //推播主題1:往前
 char* MQTTSubTopic1 = "car/carcar/forward";
 //推播主題2:往後
@@ -52,6 +52,11 @@ void forward() {  // 馬達轉向：前進
   digitalWrite(LEFT2, LOW);
   digitalWrite(RIGHT1, HIGH);
   digitalWrite(RIGHT2, LOW);
+ 
+  MQTTClient.publish(MQTTSubTopic2, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic3, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic4, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic5, String(0).c_str());
 }
 
 void backward() {  // 馬達轉向：後退
@@ -60,6 +65,11 @@ void backward() {  // 馬達轉向：後退
   digitalWrite(LEFT2, HIGH);
   digitalWrite(RIGHT1, LOW);
   digitalWrite(RIGHT2, HIGH);
+  MQTTClient.publish(MQTTSubTopic1, String(0).c_str());
+
+  MQTTClient.publish(MQTTSubTopic3, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic4, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic5, String(0).c_str());
 }
 
 void turnLeft() {  // 馬達轉向：左轉
@@ -68,6 +78,11 @@ void turnLeft() {  // 馬達轉向：左轉
   digitalWrite(LEFT2, HIGH);
   digitalWrite(RIGHT1, HIGH);
   digitalWrite(RIGHT2, LOW);
+  MQTTClient.publish(MQTTSubTopic1, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic2, String(0).c_str());
+ 
+  MQTTClient.publish(MQTTSubTopic4, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic5, String(0).c_str());
 }
 
 void turnRight() {  // 馬達轉向：右轉
@@ -76,6 +91,11 @@ void turnRight() {  // 馬達轉向：右轉
   digitalWrite(LEFT2, LOW);
   digitalWrite(RIGHT1, LOW);
   digitalWrite(RIGHT2, HIGH);
+  MQTTClient.publish(MQTTSubTopic1, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic2, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic3, String(0).c_str());
+
+  MQTTClient.publish(MQTTSubTopic5, String(0).c_str());
 }
 
 void stop() {  // 停止
@@ -84,18 +104,24 @@ void stop() {  // 停止
   digitalWrite(LEFT2, LOW);
   digitalWrite(RIGHT1, LOW);
   digitalWrite(RIGHT2, LOW);
+  MQTTClient.publish(MQTTSubTopic1, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic2, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic3, String(0).c_str());
+  MQTTClient.publish(MQTTSubTopic4, String(0).c_str());
+
+ 
 }
 
 //開始WiFi連線
 void WifiConnecte() {
   //開始WiFi連線
   WiFi.begin(ssid, password);
-  int tryCount = 0;
-
+ int tryCount=0;
+ 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    if (tryCount++ >= 20) {
+    if (tryCount ++ >=20){
       ESP.restart();
     }
   }
@@ -110,7 +136,7 @@ void MQTTConnecte() {
   MQTTClient.setCallback(MQTTCallback);
   while (!MQTTClient.connected()) {
     //以亂數為ClietID
-    String MQTTClientid = "esp32-" + String(random(1000000, 9999999));
+    String  MQTTClientid = "esp32-" + String(random(1000000, 9999999));
     if (MQTTClient.connect(MQTTClientid.c_str(), MQTTUser, MQTTPassword)) {
       //連結成功，顯示「已連線」。
       Serial.println("MQTT已連線");
@@ -120,7 +146,7 @@ void MQTTConnecte() {
       MQTTClient.subscribe(MQTTSubTopic3);
       MQTTClient.subscribe(MQTTSubTopic4);
       MQTTClient.subscribe(MQTTSubTopic5);
-
+     
 
     } else {
       //若連線不成功，則顯示錯誤訊息，並重新連線
@@ -140,7 +166,7 @@ void setup() {
   pinMode(RIGHT1, OUTPUT);
   pinMode(RIGHT2, OUTPUT);
   pinMode(RIGHT_PWM, OUTPUT);
-  //開始WiFi連線
+   //開始WiFi連線
   WifiConnecte();
 
   //開始MQTT連線
@@ -148,54 +174,13 @@ void setup() {
 }
 
 void loop() {
-  //如果WiFi連線中斷，則重啟WiFi連線
+    //如果WiFi連線中斷，則重啟WiFi連線
   if (WiFi.status() != WL_CONNECTED) WifiConnecte();
 
   //如果MQTT連線中斷，則重啟MQTT連線
-  if (!MQTTClient.connected()) MQTTConnecte();
-
-  /*
-  if (Serial.available() > 0) {
-    cmd = Serial.read();
-
-    switch (cmd) {
-      case 'w':  // 接收到'w'，前進。
-        forward();
-        run = true;  // 啟動馬達
-        break;
-      case 'x':  // 接收到'x'，後退。
-        backward();
-        run = true;  // 啟動馬達
-        break;
-      case 'a':  // 接收到'a'，左轉。
-        turnLeft();
-        run = true;  // 啟動馬達
-        break;
-      case 'd':  // 接收到'd'，右轉。
-        turnRight();
-        run = true;  // 啟動馬達
-        break;
-      case 's':
-        stop();
-        run = false;  // 停止馬達
-        break;
-    }
-  }
-
-  if (run) {
-    // 如果要啟動馬達…
-    // 向馬達輸出指定的類比電壓值
-    analogWrite(LEFT_PWM, motorSpeed);
-    analogWrite(RIGHT_PWM, motorSpeed);
-    Serial.println("100");
-  } else {
-    // 否則…
-    // 將馬達的電壓值設定成0
-    analogWrite(LEFT_PWM, 0);
-    analogWrite(RIGHT_PWM, 0);
-    Serial.println("0");
-  }
-  */
+  if (!MQTTClient.connected())  MQTTConnecte();
+ 
+  MQTTClient.loop();//更新訂閱狀態
 }
 
 
@@ -212,83 +197,26 @@ void MQTTCallback(char* topic, byte* payload, unsigned int length) {
   if (strcmp(topic, MQTTSubTopic1) == 0) {
     Serial.println("往前：" + payloadString);
     if (payloadString == "1") forward();
-    if (payloadString == "0") stop();
+    //if (payloadString == "0") stop();
   }
     if (strcmp(topic, MQTTSubTopic2) == 0) {
     Serial.println("往後：" + payloadString);
     if (payloadString == "1") backward();
-    if (payloadString == "0") stop();
+    //if (payloadString == "0") stop();
   }
     if (strcmp(topic, MQTTSubTopic3) == 0) {
     Serial.println("左轉：" + payloadString);
     if (payloadString == "1") turnLeft();
-    if (payloadString == "0") stop();
+    //if (payloadString == "0") stop();
   }
   if (strcmp(topic, MQTTSubTopic4) == 0) {
     Serial.println("右轉：" + payloadString);
     if (payloadString == "1") turnRight();
-    if (payloadString == "0") stop();
+    //if (payloadString == "0") stop();
   }
   if (strcmp(topic, MQTTSubTopic5) == 0) {
     Serial.println("停止：" + payloadString);
     if (payloadString == "1") stop();
-    if (payloadString == "0") stop();
+    //if (payloadString == "0") stop();
   }
 }
-
-// void MQTTCallback(char* topic, int* payload, unsigned int length) {
-//   Serial.print(topic);
-//   Serial.print("訂閱通知:");
-//   String payloadString;  //將接收的payload轉成字串
-//   //顯示訂閱內容
-//   for (int i = 0; i < length; i++) {
-//     payloadString = payloadString + (char)payload[i];
-//   }
-//   Serial.println(payloadString);
-//   //比對主題是否為訂閱主題1
-
-//   switch (MQTTSubTopic1) {
-//     case "1":
-//       forward();
-//       run = true;  // 啟動馬達
-//       Serial.print("往前");
-//       break;
-//     case '2':  // 接收到'x'，後退。
-//       backward();
-//       run = true;  // 啟動馬達
-//       break;
-//     case '3':  // 接收到'a'，左轉。
-//       turnLeft();
-//       run = true;  // 啟動馬達
-//       break;
-//     case '4':  // 接收到'd'，右轉。
-//       turnRight();
-//       run = true;  // 啟動馬達
-//       break;
-//     case '5':
-//       stop();
-//       run = false;  // 停止馬達
-//       break;
-//   }
-
-  // if (strcmp(topic, MQTTSubTopic1) == 0) {
-  //   Serial.println("改變燈號：" + payloadString);
-  //   if (payloadString == "1") digitalWrite(pinGLED, HIGH);
-  //   if (payloadString == "0") digitalWrite(pinGLED, LOW);
-  // }
-  // if (strcmp(topic, MQTTSubTopic2) == 0) {
-  //   Serial.println("改變燈號：" + payloadString);
-  //   if (payloadString == "1") digitalWrite(pinYLED, HIGH);
-  //   if (payloadString == "0") digitalWrite(pinYLED, LOW);
-  // }
-  // if (strcmp(topic, MQTTSubTopic3) == 0) {
-  //   Serial.println("改變燈號：" + payloadString);
-  //   if (payloadString == "1") digitalWrite(pinRLED, HIGH);
-  //   if (payloadString == "0") digitalWrite(pinRLED, LOW);
-  // }
-  // if (strcmp(topic, MQTTSubTopic4) == 0) {
-  //   Serial.println("開電扇：" + payloadString);
-  //   if (payloadString == "1") digitalWrite(Relay, HIGH);
-  //   if (payloadString == "0") digitalWrite(Relay, LOW);
-  // }
-
